7dd9b1095b373f0e03e0e56138cb140adb90c2ea
Tabzeen <talgat.abz@outlook.com>
2022-08-21 18:53:37 +0200

    Halp

--- Assets/CutsceneScript.cs
+++ Assets/CutsceneScript.cs
@@ -6,11 +6,20 @@ public class CutsceneScript : MonoBehaviour
 {   
 
     public GameObject gameController;
+
+    public GameObject[] cutsceneObjects;
     public Sprite[] browserSprites; 
+
+    public Sprite[] searchTextSprites;
+
+    public Sprite[] tabTextSprites;
+    
+
+    int iter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        iter = 0;
     }
 
     // Update is called once per frame
@@ -20,23 +29,73 @@ public class CutsceneScript : MonoBehaviour
     }
 
 
-    public void InitPhase2(){
-        Destroy(transform.GetChild(2).gameObject);
-        transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = browserSprites[1];
-        transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = true;
-        transform.GetChild(1).gameObject.SetActive(true);
+    public void InitNext(){
+        if(iter == 0){
+            transform.GetChild(2).gameObject.SetActive(false);
+            // Change to browser Results
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = browserSprites[1];
+            // Makes the Links clickable
+            transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = true;
+            // Enables the Title
+            transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else{
+
+        }
+
     }
 
+
+    // Deactivates the Tab, starts the gameController after a Link on the results page has been clicked 
     public void InitFinishCutscene(){
-        transform.GetChild(0).gameObject.SetActive(false);
-        gameController.SetActive(true);
-        gameController.gameObject.GetComponent<GameFlowController>().AcitvateLevel0();
-        
+        if(iter == 0){
+            // Disables the Search Site
+            transform.GetChild(0).gameObject.SetActive(false);
+            // Enables the Game Controller
+            gameController.SetActive(true);
+            // Starts Level 0
+            gameController.gameObject.GetComponent<GameFlowController>().AcitvateLevel0();
+
+            // Prepare for Level 1 - n
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = browserSprites[0];
+            transform.GetChild(1).GetComponent<SpriteRenderer>().sprite = tabTextSprites[1];
+            transform.GetChild(2).GetComponent<SpriteRenderer>().sprite = searchTextSprites[1];
+            iter++;
+        }
+        else{
+            
+        }
 
     }
 
-    public void EndLevel0(){
+    // Plays a cutscen when the level has ended
+    public void EndLevel(int index){
         gameController.SetActive(false);
+        if(index == 0) {
+            cutsceneObjects[0].gameObject.SetActive(true);
+        }
+        else {
+            DisplayEndCutsceneElement(1);
+        }
         
     }
+
+
+    // Plays the Cutscene for the given level
+    public void InitStartCutsceneLevel(){
+        
+    }
+
+    public void DisplayEndCutsceneElement(int e){
+        if(e < cutsceneObjects.Length-1 ){
+            cutsceneObjects[e].gameObject.SetActive(false);
+            cutsceneObjects[e+1].gameObject.SetActive(true);
+        }
+        else{
+
+        }
+    }
+
+
+
 }
